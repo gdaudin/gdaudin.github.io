9b33df518cade7f2d33e8072ac7442e1f966ceb7
Guillaume Daudin <gdaudin@mac.com>
2020-02-06 09:54:43 +0100

    Reproduction du site free

---
+++ Page_Web_de_Guillaume_Daudin/Teaching_documents_files/Teaching_documents.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20160309
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Teaching_documents_files/Teaching_documentsMoz.css')
+fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupIECSS3Opacity('id1');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
